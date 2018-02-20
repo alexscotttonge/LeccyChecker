6470c2eaa80770cac0c1d39b3792ac4a29b44746
Alex <alex@scotttonge.com>
2018-02-20 16:01:54 +0000

    replaced lazy instantiation in spec file

--- spec/meter_reader_spec.rb
+++ spec/meter_reader_spec.rb
@@ -7,7 +7,7 @@ describe MeterReader do
     end
   end
 
-  let(:meter_reader) { described_class.new }
+  subject(:meter_reader) { described_class.new }
 
     it 'takes an input from STDIN' do
       allow(meter_reader).to receive(:gets).and_return(123456)
@@ -25,5 +25,7 @@ describe MeterReader do
       meter_reader.user_input
       expect { meter_reader.check_length }.to raise_error(ErrorLibrary::InputLengthError)
     end
+
+
 
 end
