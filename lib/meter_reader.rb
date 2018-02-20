8a489c9fb8bcd15c4bb6dbac0a788e3f97e3eb5d
Alex <alex@scotttonge.com>
2018-02-20 15:54:40 +0000

    raises error if input length is less than four digits

--- lib/meter_reader.rb
+++ lib/meter_reader.rb
@@ -6,6 +6,12 @@ class MeterReader
     @input = gets.to_i
   end
 
+  def check_length
+    if @input.digits.count < 4
+      raise InputLengthError, 'Your number must be between 4 and 6 digits long'
+    end
+  end
+
   def new_reading
     CURRENT_READING + @input
   end
--- spec/meter_reader_spec.rb
+++ spec/meter_reader_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe MeterReader do
 
+  class InputLengthError < StandardError
+  end
+
   let(:meter_reader) { described_class.new }
 
     it 'takes an input from STDIN' do
@@ -13,6 +16,12 @@ describe MeterReader do
       allow(meter_reader).to receive(:gets).and_return(2000)
       meter_reader.user_input
       expect(meter_reader.new_reading).to eq 10000
+    end
+
+    it 'raises an error if the input length is less than four characters' do
+      allow(meter_reader).to receive(:gets).and_return(435)
+      meter_reader.user_input
+      expect { meter_reader.check_length }.to raise_error(InputLengthError)
     end
 
 end
